a4ec7b4697f88d977e0fdd4d1a230c478eb2d051
bomeng <bmeng@us.ibm.com>
2016-11-11 20:29:24 -0800

    fix custom filter test cases

--- src/test/scala/org/apache/spark/sql/hbase/HBaseCFTestSuite.scala
+++ src/test/scala/org/apache/spark/sql/hbase/HBaseCFTestSuite.scala
@@ -70,18 +70,12 @@ class HBaseCFTestSuite extends TestBase {
      * create table
      */
     val createSql =
-      s"""CREATE TABLE cf(
-        k1 INTEGER,
-        k2 INTEGER,
-        k3 INTEGER,
-        nk1 INTEGER,
-        nk2 INTEGER,
-        PRIMARY KEY(k1, k2, k3))
-        MAPPED BY
-        (cf_htable, COLS=[
-          nk1=f.nk1,
-          nk2=f.nk2
-        ])""".stripMargin
+      s"""CREATE TABLE cf TBLPROPERTIES(
+          'hbaseTableName'='cf_htable',
+          'colsSeq'='k1,k2,k3,nk1,nk2',
+          'keyCols'='k1,INTEGER;k2,INTEGER;k3,INTEGER',
+          'nonKeyCols'='nk1,INTEGER,f,nk1;nk2,INTEGER,f,nk2')"""
+        .stripMargin
 
     try {
       runSql(createSql)
