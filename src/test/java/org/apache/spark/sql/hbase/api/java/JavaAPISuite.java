1afc3bda0ede998b65942044ca81373d6de46000
yzhou2001 <yzhou_1999@yahoo.com>
2016-11-19 13:26:51 -0800

    Add back Java API test

---
+++ src/test/java/org/apache/spark/sql/hbase/api/java/JavaAPISuite.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.hbase.api.java;
+
+import org.apache.hadoop.hbase.HBaseTestingUtility;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.hbase.HBaseSparkSession;
+import org.apache.spark.sql.hbase.TestBase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.Serializable;
+
+public class JavaAPISuite extends TestBase implements Serializable {
+    private transient SparkSession ss;
+    private transient HBaseTestingUtility testUtil = null;
+    private transient JavaSparkContext sc = null;
+
+    @Before
+    public void setUp() {
+      System.setProperty("spark.hadoop.hbase.zookeeper.quorum", "localhost");
+      SparkConf scf = new SparkConf(true);
+      sc = new JavaSparkContext("local", "JavaAPISuite", scf);
+      ss = new HBaseSparkSession(sc);
+      testUtil = new HBaseTestingUtility(ss.sparkContext().hadoopConfiguration());
+      try {
+        testUtil.cleanupTestDir();
+        testUtil.startMiniZKCluster();
+        testUtil.startMiniHBaseCluster(1, 1);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+
+    @After
+    public void tearDown() {
+      try {
+        testUtil.shutdownMiniHBaseCluster();
+        testUtil.shutdownMiniZKCluster();
+        testUtil.cleanupTestDir();
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+      ss = null;
+      sc.stop();
+      sc = null;
+    }
+
+    @Test
+    public void testCreateInsertRetrieveTable() {
+        final String hb_staging_table = "HbStagingTable";
+        final String staging_table = "StagingTable";
+        final String insert_sql = "INSERT INTO TABLE " + staging_table + " VALUES (\"strcol\" , \"bytecol\" , \"shortcol\" , \"intcol\" ," +
+                "  \"longcol\" , \"floatcol\" , \"doublecol\")";
+        final String retrieve_sql = "SELECT * FROM " + staging_table;
+        String create_sql = "CREATE TABLE " + staging_table + " TBLPROPERTIES(" +
+                "'hbaseTableName'='" + hb_staging_table +"'," +
+                "'cols'='strcol,bytecol,shortcol,intcol,longcol,floatcol,doublecol'," +
+                "'keyCols'='doublecol,STRING;strcol,STRING;intcol,STRING'," +
+                "'nonKeyCols'='bytecol,STRING,cf1,hbytecol;" +
+                "shortcol,STRING,cf1,hshortcol;longcol,STRING,cf2,hlongcol;floatcol,STRING,cf2,hfloatcol')";
+        ss.sql(create_sql).collect();
+        ss.sql(insert_sql).collect();
+        org.apache.spark.sql.Dataset<Row> df = ss.sql(retrieve_sql);
+        Row row = df.head();
+
+        assert (row.toString().equals("[strcol,bytecol,shortcol,intcol,longcol,floatcol,doublecol]"));
+    }
+}
